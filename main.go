a99fd679588707efe8c7f5a6c6b60e08335c9e1b
Haibin Liu <liu.haibin@gmail.com>
2015-09-26 14:07:41 +0800

    Add web interface

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 
 	_ "github.com/lib/pq"
 )
@@ -15,17 +16,38 @@ type Book struct {
 	price  float32
 }
 
-func main() {
+// TODO: remove the global
+var db *sql.DB
+
+func init() {
+	var err error
 	// a pool of connections; safe for concurrent access
-	db, err := sql.Open("postgres", "postgres://haibin@localhost/bookstore?sslmode=disable")
+	db, err = sql.Open("postgres", "postgres://haibin@localhost/bookstore?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/books", booksIndex)
+	http.ListenAndServe(":3000", nil)
+}
+
+func booksIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
 	}
 
 	// fetch a resultset
 	rows, err := db.Query("SELECT * FROM books")
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
 	defer rows.Close()
 
@@ -35,15 +57,17 @@ func main() {
 		bk := new(Book)
 		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
 		if err != nil {
-			log.Fatal(err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
 		}
 		bks = append(bks, bk)
 	}
 	if err = rows.Err(); err != nil {
-		log.Fatal(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
 
 	for _, bk := range bks {
-		fmt.Printf("%s, %s, %s, £%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
+		fmt.Fprintf(w, "%s, %s, %s, £%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
 	}
 }
